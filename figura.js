37b5baf048a29762a5889129717e3d1008c8582a
christian moreno <christianmorenoti@gmail.com>
2021-12-01 23:36:52 -0500

    Agregando funciones en operaciones aritmeticas

--- figura.js
+++ figura.js
@@ -1,38 +1,45 @@
-console.log('Hola desde JS');
+console.group("cuadrado");
+const ladoCuadrado = (lado) => lado;
+console.log(`Los lados del cuadrado miden: ${ladoCuadrado(2)}cm`);
 
-console.group('cuadrado')
+const perimetroCuadrado = (lado) => lado * 4;
+console.log(`El perimetro del cuadrado es: ${perimetroCuadrado(5)}cm`);
 
-const ladoCuadrado = 5;
-
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado}cm`)
-
-
-const perimetroCuadrado = ladoCuadrado * 4;
-console.log(`El perimetro del cuadrado es ${perimetroCuadrado}cm`)
-
-const areaCuadrado = ladoCuadrado * ladoCuadrado; 
-console.log(`El area del cuadrado es: ${areaCuadrado}cm^2`)
-
-console.groupEnd();
-console.group('Triangulos')
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 12;
-const alturaTriangulo = 5.5;
-
-console.log(`Los lados del triangulo miden ${ladoTriangulo1} cm, 
-y ${ladoTriangulo2} respectivamente y la base ${baseTriangulo}`)
-
-console.log(`La altura del tringulo es de ${alturaTriangulo}`);
-
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-console.log(`El perimetro del triangulo es ${perimetroTriangulo}`);
-
-const areaTringulo = (baseTriangulo * alturaTriangulo) / 2;
-console.log(`El area del tringulo es ${areaTringulo}`);
+const areaCuadrado = (lado) => lado * lado;
+console.log(`El area del cuadrado es: ${areaCuadrado(10)} cm^2`);
 console.groupEnd();
 
+console.group("Triangulos");
+const ladoBaseTringulo = (lado1, lado2, base) => {
+  return console.log(
+    `Los lados del triangulo son ${lado1} y ${lado2} cm. la base es de ${base} cm`
+  );
+};
+ladoBaseTringulo(2, 2, 5);
 
-console.group('circulo');
+const alturaTriangulo = (altura) => altura;
+console.log(`La altura del tringulo es de ${alturaTriangulo(2)} cm`);
 
+const perimetroTriangulo = (lado1, lado2, base) => lado1 + lado2 + base;
+console.log(`El perimetro del triangulo es ${perimetroTriangulo(3, 5, 10)}`);
+
+const areaTringulo = (base, altura) => (base * altura) / 2;
+console.log(`El area del triangulo es ${areaTringulo(10, 5)}`);
+console.groupEnd();
+
+console.group("circulo");
+const PI = Math.PI;
+console.log(`PI es igual a ${PI} `);
+
+const radioCirculo = (radio) => radio;
+console.log(`El radio del circulo es ${radioCirculo(2)}`);
+
+const diametroCirculo = (radio) => radio * 2;
+console.log(`El diametro del circulo es ${diametroCirculo(2)}`);
+
+const perimetroCirculo = (diametro) => diametro * PI;
+console.log(`El perimetro del circulo es ${perimetroCirculo(2)}`);
+
+const areaCirculo = (radio) => radio * radio * PI;
+console.log(`El area del circulo es ${areaCirculo(2)}`);
 console.groupEnd();
